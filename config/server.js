5e93083ea366fff6617a0a5f0609e513fb5f9c1b
Kseylo <alekseysarychev29@gmail.com>
2024-04-27 14:45:54 +0700

    updated ulr

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://localhost/poh_event',
+  url: 'https://bx24-ext.mdrk.ru/poh_event/',
   app: {
     keys: env.array('APP_KEYS'),
   },
