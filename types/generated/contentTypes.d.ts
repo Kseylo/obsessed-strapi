9bd5d05a1f73e77ee7c5abd76455e17d47833ca5
Kseylo <alekseysarychev29@gmail.com>
2024-05-14 10:04:09 +0700

    Changed schema

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -873,8 +873,8 @@ export interface ApiQuestQuest extends Schema.CollectionType {
     illustration: Attribute.Media & Attribute.Required;
     backgroundColor: Attribute.String &
       Attribute.CustomField<'plugin::color-picker.color'>;
-    rules: Attribute.Blocks & Attribute.Required;
-    description: Attribute.Blocks;
+    rules: Attribute.Blocks;
+    description: Attribute.Blocks & Attribute.Required;
     date: Attribute.String;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
